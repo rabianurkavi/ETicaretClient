dcb11a99eb9a7d41c13cd382508cd6ba32093e93
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-03-21 12:23:06 +0300

    Toastr Library

--- ETicaretUI/src/app/app.component.ts
+++ ETicaretUI/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $: any
 
 @Component({
@@ -8,4 +10,23 @@ declare var $: any
 })
 export class AppComponent {
   title = 'ETicaretUI';
+  constructor(private toastrService:CustomToastrService){
+    toastrService.message("Merhaba","Rabia",{
+      messageType: ToastrMessageType.Info,
+      position:ToastrPosition.TopRight
+    })
+    toastrService.message("Merhaba","Rabia",{
+      messageType: ToastrMessageType.Success,
+      position:ToastrPosition.TopRight
+    })
+    toastrService.message("Merhaba","Rabia",{
+      messageType: ToastrMessageType.Warning,
+      position:ToastrPosition.TopRight
+    })
+    toastrService.message("Merhaba","Rabia",{
+      messageType: ToastrMessageType.Error,
+      position:ToastrPosition.TopRight
+    })
+
+  }
 }
